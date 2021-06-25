822c60a16cd26d6c7fd0be8ce27dbaa965b4239d
vinuthnagrace <grace.m@sparktics.com>
2021-06-25 12:18:53 +0530

    Day 2 modifications

--- Day1Exercise.js
+++ Day1Exercise.js
@@ -1,56 +1,56 @@
 //1. Find the Smallest Number from the given three numbers?
 // smallestNumber(12,6,9) => 6
-// let a = 12;
-// b = 6;
-// c = 9;
-// if (a <= b && a <= c) {
-//   document.write(a);
-// } else if (b <= a && b <= c) {
-//   document.write(b);
-// } else {
-//   document.write(c);
+// function smallNumber(a, b, c) {
+//   if (a < b && a < c) {
+//     return a;
+//   } else if (b < c) {
+//     return b;
+//   } else {
+//     return c;
+//   }
 // }
+// document.write(smallNumber(12, 6, 9));
 
 //2. Find the Largest Number from the given three numbers?
 // largestNumber(3,5,15) => 15
-// let a = 3;
-// b = 5;
-// c = 15;
-// if (a >= b && a >= c) {
-//   document.write(a);
-// } else if (b >= a && b >= c) {
-//   document.write(b);
-// } else {
-//   document.write(c);
+// function largestNumber(a, b, c) {
+//   if (a > b && a > c) {
+//     return a;
+//   } else if (b > c) {
+//     return b;
+//   } else {
+//     return c;
+//   }
 // }
+// document.write(largestNumber(3, 5, 15));
 
 //3. Find the sum of two numbers?
 // sum(4,5) => 9
-// let a = 4;
-// b = 5;
-// c = a + b;
-// document.write(c);
+// function sum(a, b) {
+//   return a + b;
+// }
+// document.write(sum(4, 5));
 
 //4. Find the difference between two numbers?
 // subtraction(3,9) => -6
-// let a = 3;
-// b = 9;
-// c = a - b;
-// document.write(c);
+// function sub(a, b) {
+//   return a - b;
+// }
+// document.write(sub(3, 9));
 
 //5. Find the multiplication of two numbers?
 // multiplication(4,6) => 24
-// let a = 4;
-// b = 6;
-// c = a * b;
-// document.write(c);
+// function multi(a, b) {
+//   return a * b;
+// }
+// document.write(multi(4, 6));
 
 //6. Find the division of two numbers?
 // division(9,3) => 3
-// let a = 9;
-// b = 3;
-// c = a / b;
-// document.write(c);
+// function divd(a, b) {
+//   return a / b;
+// }
+// document.write(divd(9, 3));
 
 // 7.Print the below format, using loops
 //  #
@@ -79,54 +79,77 @@
 // hash(7);
 
 //8.Function to find the area of a triangle.
-// areaTriangle(4,5,6) => 9.92
+// areaTriangle(4,5) => 10
 // formula : s=side1+side2+side3/2 area of triangle = sqrt s(s-a)(s-b)(s-c)
-// let b = 4;
-// h = 5;
-// result = (b * h) / 2;
-// document.write(result);
+// function tri(h, b) {
+//   return (b * h) / 2;
+// }
+// document.write(tri(4, 5));
 
 // 9.Function to find the area of the square.
 // areaSquare(4) => 16
-// let a = 4;
-// square = a * a;
-// document.write(square);
+// function sqr(a) {
+//   return a * a;
+// }
+// document.write(sqr(4));
 
 // 10.Function to find the area of rectangle
 // areaRectangle(3,4) => 24
-// let length = 3;
-// width = 4;
-// area = length * width;
-// document.write(area);
+// function rect(l, w) {
+//   return l * w;
+// }
+// document.write(rect(3, 4));
 
 // 11.Find the sum of n numbers.
 // sumNumbers(5) => 15
-// n*(n+1)/2
-// let a = 5;
-// b = (a * (a + 1)) / 2;
-// document.write(b);
+// function sum(n) {
+//   return (n * (n + 1)) / 2;
+// }
+// document.write(sum(5));
 
 // 12.Print all the even numbers up to and including n.
 // evenNumbers(9) => 2,4,6,8,9
-
-// for (i = 1; i <= 9; i++) {
-//   if (i % 2 == 0) {
-//     document.write(i);
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// function even(i) {
+//   for (i = 1; i <= 9; i++) {
+//     if (i % 2 == 0) {
+//       document.write(i);
+//     }
 //   }
 // }
+// even(arr);
 
 //13. Print all the values of Array.
 // getArray([1,2,3,4]) => 1,2,3,4
 // let array = [1, 2, 3, 4];
+// function print() {
+//   return;
+// }
 // document.write(array);
 
 //14. Print all the values of Array in Reverse order.
 // getReverseArray([1,2,3,4]) => 4,3,2,1
+// let myArray = [1, 2, 3, 4];
+// function reverse(i) {
+//   let a = 0;
+//   let b = i.length - 1;
+//   while (a < b) {
+//     let c = i[a];
+//     i[a] = i[b];
+//     i[b] = c;
+//     a++;
+//     b--;
+//   }
+// }
+// reverse(myArray);
+// document.write(myArray);
+
 // let arr = [1, 2, 3, 4];
-// b = arr.reverse();
+// let b = arr.reverse()
 // document.write(b);
 
 //15. Find the sum of all numbers in an Array.
+// 0 - initial value - 1st call of the call back should be 0 in addition and 1 in multiplication
 // sumArray[1,2,3,4] => 10
 // let array = [1, 2, 3, 4];
 // let sum = array.reduce(function (a, b) {
